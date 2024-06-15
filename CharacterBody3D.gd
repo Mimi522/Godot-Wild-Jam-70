bd1398a34e3f816435e4faf404496a488a5353a5
Milena Mariz <milenaferreira294@hotmail.com>
2024-06-15 14:01:27 -0300

    feat: better jump!

--- CharacterBody3D.gd
+++ CharacterBody3D.gd
@@ -1,7 +1,8 @@
 extends CharacterBody3D
 
-const SPEED = 15
-const JUMP_VELOCITY = 15
+@export var speed = 15
+@export var fall_acceleration = 75
+@export var jump_impulse = 20
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -9,21 +10,21 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y -= gravity * delta * 5
+		velocity.y = velocity.y - (fall_acceleration * delta)
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jump_impulse
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("move_left", "move_right", "move_foward", "move_back")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
 
 	move_and_slide()
